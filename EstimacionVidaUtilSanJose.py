061e6806931f6d073b93d4ff39bc26697ca68185
Yan <135562876+YanmirG@users.noreply.github.com>
2025-06-11 20:46:31 -0500

    Update EstimacionVidaUtilSanJose.py

--- EstimacionVidaUtilSanJose.py
+++ EstimacionVidaUtilSanJose.py
@@ -30,10 +30,15 @@ data = pd.get_dummies(data, columns=['tipo_producto'], drop_first=True)
 X = data.drop(columns=['vida_util_dias'])
 y = data['vida_util_dias']
 
-# Dividir en entrenamiento y prueba
+# ---------------------------------------------------
+# 4) Dividir en entrenamiento y prueba
+# ---------------------------------------------------
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Modelo de regresión
+# ---------------------------------------------------
+# 5) Entrenar el modelo de regresión
+# --------------------------------------------------
 modelo = LinearRegression()
 modelo.fit(X_train, y_train)
 
